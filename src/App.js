ad186ac9366adc75ec4cc8eba0f4f5f2763a1311
Richard <leitw99@uw.edu>
2022-02-15 14:02:39 -0800

    Gif Search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import TextFile from Material UI
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [text, setText] = useState("");
+  const [gifs, setGifs] = useState([]);
+
+  async function search() {
+    const key = "V9Xu2YHeMWSVi3bhGN0buXSowlxWCf3w";
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${text}&limit=25&offset=0&lang=en`;
+    const r = await fetch(url);
+    const j = await r.json();
+    console.log(j);
+    setGifs(j.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchbar">
+        {/*  "outlined" variant prop gives the nice animation */}
+        <TextField
+          id="search"
+          label="Search"
+          variant="outlined"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") search();
+          }}
+        />
+        <Button variant="outlined" onClick={search} size="large">
+          Search
+        </Button>
+      </div>
+      <div className = "gifs">
+        {gifs.map((gif, i) => {
+          return <img key = {i} src = {gif.images.fixed_height.url}/>;
+          })}
+      </div>
     </div>
   );
 }
